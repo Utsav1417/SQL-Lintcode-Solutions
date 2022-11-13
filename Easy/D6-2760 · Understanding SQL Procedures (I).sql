564e09bf68f094d6966c84a4651c17645672c1ab
Utsav1417 <utsavspanchal2001@gmail.com>
2022-11-13 14:17:40 +0530

    Day-6

---
+++ Easy/D6-2760 · Understanding SQL Procedures (I).sql
@@ -0,0 +1,37 @@
+-- 2760 · Understanding SQL Procedures (I)
+
+-- Description
+-- We have provided a SQL procedure 'getTeachers', please write SQL statement to call this procedure to see its content.
+
+-- Example
+-- Input
+
+-- teachers：
+
+-- id	name	email	age	country
+-- 1	'Eastern heretic'	'eastern.heretic@gmail.com'	20	'UK'
+-- 2	'Northern Beggar'	'northern.beggar@qq.com'	21	'CN'
+-- 3	'Western Venom'	'western.venom@163.com'	18	'USA'
+-- 4	'Southern Emperor'	'southern.emperor@qq.com'	21	'JP'
+-- Return：
+
+-- id	name	email	age	country
+-- 1	'Eastern heretic'	'eastern.heretic@gmail.com'	20	'UK'
+-- 2	'Northern Beggar'	'northern.beggar@qq.com'	21	'CN'
+-- 3	'Western Venom'	'western.venom@163.com'	18	'USA'
+-- 4	'Southern Emperor'	'southern.emperor@qq.com'	21	'JP'
+
+ 
+ 
+-- =================================================================================
+-- =================================================================================
+ 
+ 
+-- My Solution
+call getTeachers;
+ 
+-- =================================================================================
+-- =================================================================================
+ 
+ 
+-- Others Solution
